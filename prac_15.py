3180196b3c7c6e108d100dfb1c923c1c3b1d9842
GaneshWaje003 <118741084+GaneshWaje003@users.noreply.github.com>
2023-03-28 10:06:25 +0530

    Update prac_15.py
    disabling the comments

--- prac_15.py
+++ prac_15.py
@@ -1,35 +1,38 @@
-# Create a class Employee with data members: name, department and salary. Create
-# suitable methods for reading and printing employee information
+'''
+Create a class Employee with data members: name, department and salary. Create
+suitable methods for reading and printing employee information
+'''
 
-# class Employee:
+class Employee:
  
-#     def getInfo(self):
-#         a = input("Enter your name :")
-#         b = input("Enter your department :")
-#         c = int(input("Enter your salary :"))
+    def getInfo(self):
+        a = input("Enter your name :")
+        b = input("Enter your department :")
+        c = int(input("Enter your salary :"))
         
-#         print("\n\nMR.",a,"\nyour Department is : ",b,"\nWith Salary is : ",c,"\n")
+        print("\n\nMR.",a,"\nyour Department is : ",b,"\nWith Salary is : ",c,"\n")
         
-# obj = Employee()
-# obj.getInfo()
+ 
+obj = Employee()
+obj.getInfo()
 
 # # Python program to read and print students information using two classes using
 # # simple inheritance.
 
-# class g1:
+class g1:
 
-#     def __init__(self):
-#         ba= input("Enter your name :")
-#         bb = input("Enter your stream :")
-#         bc = input("Enter your roll number :")
+     def __init__(self):
+         ba= input("Enter your name :")
+         bb = input("Enter your stream :")
+         bc = input("Enter your roll number :")
         
-#         print("\n\nName :",ba,"\tStream :",bb,"\tRoll no. :",bc,"\n")
+         print("\n\nName :",ba,"\tStream :",bb,"\tRoll no. :",bc,"\n")
         
-# class g2(g1):
-#     print()
+class g2(g1):
+     print()
    
     
-# obj = g2()
+obj = g2()
 
 # Write a Python program to implement multiple inheritance
 
